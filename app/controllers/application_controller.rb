e5fc8e97fe24511e5651520ebb138574cc95c858
brian <brian.mtr@outlook.fr>
2021-02-17 14:34:07 +0100

    translate devise to fr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
     before_action :configure_devise_parameters, if: :devise_controller?
+    around_action :switch_locale
+
     def configure_devise_parameters
         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation) }
-    end   
+    end    
+
+    
+    def switch_locale(&action)
+        locale = params[:locale] || I18n.default_locale
+        I18n.with_locale(locale, &action)
+    end
+
+    def default_url_options
+        { locale: I18n.locale }
+    end
+
 end
