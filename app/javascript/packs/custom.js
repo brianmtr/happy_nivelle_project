722e99e799617d37a17926fc84085e73f8ccc334
brian <brian.mtr@outlook.fr>
2021-02-23 12:31:48 +0100

    add front to event card

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -7,10 +7,14 @@
     let hamButton = document.querySelector('.ham');
     let navBar = document.querySelector('.nav-ham');
     hamButton.addEventListener('click', openMenu);function openMenu(){
+       
         navBar.classList.toggle('showNav');
         hamButton.classList.toggle('showClose');
-      }let menuLinks = document.querySelectorAll('.menuLink');menuLinks.forEach(
+      }
+      let menuLinks = document.querySelectorAll('.menuLink');
+      menuLinks.forEach(
         function(menuLink){
           menuLink.addEventListener('click', openMenu);
+      
         }
       )
