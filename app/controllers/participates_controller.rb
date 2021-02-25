6a51ee4bea3d1dbc4adce9a498c989327ac69dbe
Bruno Bastin <bruno59490@hotmail.fr>
2021-02-25 19:16:31 +0100

    testing upsert_all in the functionality

--- app/controllers/participates_controller.rb
+++ app/controllers/participates_controller.rb
@@ -1,12 +1,15 @@
 class ParticipatesController < EventsController
 before_action :set_event, only: %i[ update ]
 before_action :set_userevent, only: %i[ new create ]
+before_action :set_user
 
   # PATCH/PUT /events/1 or /events/1.json
   def update
     if @event.increment!(:participate)
     @userevent = Userevent.upsert(
-      state: "yes"
+      state: "yes",
+      events_id: @event.id,
+      users_id: current_user.id
     )
     end
   end
@@ -15,6 +18,10 @@ before_action :set_userevent, only: %i[ new create ]
   # Use callbacks to share common setup or constraints between actions.
   def set_event
     @event = Event.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def set_userevent
