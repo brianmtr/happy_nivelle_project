8456eefba4b46a38d7f1ecee6bc5efe1cb211ade
Bruno Bastin <bruno59490@hotmail.fr>
2021-02-25 19:00:13 +0100

    testing .upsert for functionality participate

--- app/controllers/participates_controller.rb
+++ app/controllers/participates_controller.rb
@@ -5,9 +5,9 @@ before_action :set_userevent, only: %i[ new create ]
   # PATCH/PUT /events/1 or /events/1.json
   def update
     if @event.increment!(:participate)
-    @userevent = Userevent.new(userevent_params)
-    @userevent.state  = 'yes'
-    @userevent.save
+    @userevent = Userevent.upsert(
+      state: "yes"
+    )
     end
   end
 
