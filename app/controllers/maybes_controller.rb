6543387bb7d5d1ea5c3d6db6bc50821dbe462b20
Bruno Bastin <bruno59490@hotmail.fr>
2021-03-02 11:18:56 +0100

    adjusting incrementation and decrementation for events

--- app/controllers/maybes_controller.rb
+++ app/controllers/maybes_controller.rb
@@ -8,26 +8,33 @@ class MaybesController < EventsController
       user = current_user
       event = Event.find(params[:id])
 
-      if UserEvent.where(:state => 'yes')
+      if UserEvent.where(state: 'yes', user_id: user, event_id: event).exists?
         @event.decrement!(:participate)
       end
       
-      if UserEvent.where(:state => 'no')
+      if UserEvent.where(state: 'no', user_id: user, event_id: event).exists?
         @event.decrement!(:not_participate)
       end
 
       if @event.increment!(:maybe_participate)
-        if UserEvent.nil?
-          user_event = UserEvent.where(user: current_user, event: @event).update(state: 'maybe')
-      else UserEvent.where(user: current_user, event: @event).exists?
-          user_event = UserEvent.new
-          user_event.user = user
-          user_event.event = event
-          user_event.save
-          user_event.maybe!
-        
+        if UserEvent.where(user_id: user, event_id: event).exists?
+          UserEvent.where(user: current_user, event: @event).update(state: 'maybe')
+        else
+          UserEvent.where(user: user, event: event).upsert(state: "maybe", user_id: user, event_id: event) 
         end
       end
+      # if @event.increment!(:maybe_participate)
+      #   if UserEvent.nil?
+      #     user_event = UserEvent.where(user: current_user, event: @event).update(state: 'maybe')
+      # else UserEvent.where(user: current_user, event: @event).exists?
+      #     user_event = UserEvent.new
+      #     user_event.user = user
+      #     user_event.event = event
+      #     user_event.save
+      #     user_event.maybe!
+        
+      #   end
+      # end
     end
   
       private
