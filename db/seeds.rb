515bbe8e5f4dfb15fa1139e46bd5a2fce5909bbf
Bruno Bastin <bruno59490@hotmail.fr>
2021-03-03 10:54:02 +0100

    removing incrementation and decrementation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,27 +10,28 @@
 
 # delete all entries for moment, i don't use environnement
 
-
 # create 10 users
 (1..10).each do |_number|
-  User.create firstname:   Faker::Name.name,
-              lastname:   Faker::Name.name,
-              email:  Faker::Internet.email,
-              password: Faker::Lorem.characters(number: 10),
-              password_confirmation: Faker::Lorem.characters(number: 10)
-    @user = User.all.sample
-    @users = User.all.shuffle
-    @users.each {|u| u.update_attribute(admin: true) }
+  password = Faker::Lorem.characters(number: 10)
+  User.create firstname:              Faker::Name.name,
+              lastname:               Faker::Name.name,
+              email:                  Faker::Internet.email,
+              password:               password,
+              password_confirmation:  password
 end
-
-
-
-
+​
+(1..5).each do |_number|
+  @user = User.all.sample
+  @user.update_attribute(:admin, 'true')
+end
+​ 
 # creation des events en attibuant un user, et un nombre de tag à chaque.
-(1..10).each do |_number|
-  @event = Event.create title:        Faker::Lorem.sentence,
-                        description:      Faker::Lorem.paragraph(sentence_count: 3),
-                        address:        Faker::Address.full_address,
-                        status:        Faker::Number.between(from: 0, to: 2),
-                        date:        Faker::Date.between(from: 2.month.ago, to: 2.month.from_now)
+(1..50).each do |_number|
+  @event = Event.new  title:        Faker::Lorem.sentence,
+                      description:  Faker::Lorem.paragraph(sentence_count: 3),
+                      address:      Faker::Address.full_address,
+                      status:       rand(0..2),
+                      date:         Faker::Date.between(from: 2.month.ago, to: 2.month.from_now)
+  # @event.image_data.attach(io: File.open("#{Rails.root}/public/smiley-2403201_1920.png"), filename: 'image.png', content_type: 'image/png')
+  @event.save
 end
