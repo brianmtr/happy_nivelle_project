12474f3fcd13dece6285c4a213e98049ee77ad56
Bruno Bastin <bruno59490@hotmail.fr>
2021-03-09 13:08:10 +0100

    debug registrations and adding rgpd field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
     
     def configure_devise_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation) }
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :rgpd) }
     end    
 
     
