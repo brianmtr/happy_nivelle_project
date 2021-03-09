67cfa6886acb7dda9997ae1d3925e8de6c6c584a
Bruno Bastin <bruno59490@hotmail.fr>
2021-03-09 11:06:59 +0100

    debug trad errors and show event past in home view and its controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @events_in_progress = Event.accepted.where("date >= ?", Date.today).limit(3)
-    @events_proposed = Event.proposed.where("date >= ?", Date.today).limit(3)
-    @events_past = Event.past.where("date <= ?", Date.today).limit(3)
+    @events_proposed    = Event.proposed.where("date >= ?", Date.today).limit(3)
+    @events_past        = Event.past.where('date < ?', Date.today).limit(3)
+    if Event.accepted.where('date < ?', Date.today).update(status: 'past')
+    end
   end
 end
--- app/controllers/validate_controller.rb
+++ app/controllers/validate_controller.rb
@@ -1,40 +1,29 @@
 class ValidateController < EventsController
-    before_action :set_event, only: %i[ show index]
 
-
-    def index    
-        @events = Event.all
-        @events_in_progress = Event.accepted.where('date >= ?', Date.today).limit(3)
-        @events_proposed    = Event.proposed.where('date >= ?', Date.today).limit(3)
-        @events_past        = Event.accepted.where('date <= ?', Date.today).limit(3)
-      end
-      
-      def validate
+def validate
     @event = Event.find(params[:id])
     status = @event.status 
-    case status
-when nil
-    @event.status = 'proposed'
-    @event.save
-    redirect_to '/events/', notice: "l'évenement a bien été accepté."
+  case status
+    when nil
+      @event.status = 'proposed'
+      @event.save
+      redirect_to '/events/', notice: "l'évenement a bien été accepté."
   
-  when 'proposed' 
-    @event.status = 'accepted'
-    @event.save
+    when 'proposed' 
+     @event.status = 'accepted'
+      @event.save
+      redirect_to '/events/', notice: "l'évenement a bien été accepté."
 
-  when 'accepted' && Date.today >= @event.date
-    @event.status = 'past'
-    @event.save
-    redirect_to '/events/', notice: "l'évenement a bien été accepté."
-    end
+    when 'accepted'
+      @event.status = 'past'
+      @event.save
+      redirect_to '/events/', notice: "l'évenement a bien été accepté."
   end
+end
 
 
 private
 
-def set_event
-    @event = Event.find(params[:id])
-  end
 
 def event_params
     params.require(:event).permit(:title, :date, :adress, :description, :image)
