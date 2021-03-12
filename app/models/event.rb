54fc22f537823e665d340fa7cb68105620be603b
brian <brian.mtr@outlook.fr>
2021-03-12 14:00:24 +0100

    front to title event

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
     has_one_attached :image
     
     validates :title, presence: true, length: {minimum:2, maximum:12}
-    validates :description, presence: true
+    validates :description, presence: true, length: {minimum:2, maximum:1000}
     validates :address, presence: true
     validates :date, presence: true
     validates :image, attached: true, content_type: %i[png jpg jpeg]
